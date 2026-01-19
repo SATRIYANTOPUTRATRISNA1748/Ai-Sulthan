0450a855be9af13672e25c08e064ac3e126ed722
Satriyanto Putra Trisna <159920323+SATRIYANTOPUTRATRISNA1748@users.noreply.github.com>
2026-01-19 20:58:56 +0700

    data.js

--- frontend/data.js
+++ frontend/data.js
@@ -1,6 +1,6 @@
 (async () => {
-    const module = await import(` http://10.242.59.159:5000/app.js`);
+    const module = await import(` http://10.242.59.159:5000/app.js`); // Change Your API
     module.initChat(); 
 })();
 
-// Ngapain sih LOL buka Source code web gw penasaran ya? Makanya kalau lagi kelas math & physic jangan kabur-kaburan dan tidur di kelas Kan jadi GOBLOK
+
